2051a51554f7ad0fe4ff8a833369ba262ad85f64
ashok bathu <ashokb@a2kitsolutions.com>
2017-08-20 12:55:49 +0530

    elastic Ip is added

--- assistant/services/doctors.js
+++ assistant/services/doctors.js
@@ -3,8 +3,8 @@ var myapp = angular.module('doctorServices', []);
 myapp.service('doctorServices', ['$http', function($http) {
     var getDoctors = function() {
         console.log("in doctors service");
-        console.log("doctors--", $http.get('http://54.191.212.94:8080/dbotica-spring/assistant/getMyDoctors'));
-        return $http.get('http://54.191.212.94:8080/dbotica-spring/assistant/getMyDoctors');
+        console.log("doctors--", $http.get('http://34.211.209.156:8080/dbotica-spring/assistant/getMyDoctors'));
+        return $http.get('http://34.211.209.156:8080/dbotica-spring/assistant/getMyDoctors');
 
     }
     return {
